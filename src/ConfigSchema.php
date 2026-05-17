56149eef24a83caeab489056a738d3d92c386e76
Timothy McClatchey <timothy@kansatech.com>
2026-05-16 22:27:17 -0500

    General scaffolding

---
+++ src/ConfigSchema.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommonPHP\Drivers\Database\MSSQL;
+
+use CommonPHP\Config\Contracts\AbstractConfigSchema;
+
+class ConfigSchema extends AbstractConfigSchema
+{
+
+}
